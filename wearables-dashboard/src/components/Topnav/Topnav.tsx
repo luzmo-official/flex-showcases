48c8bda35e6260a014fea3d00c51704b8a63f67d
joost-stessens <122776799+joost-stessens@users.noreply.github.com>
2024-09-02 11:31:50 +0200

    Chore: improve topnav styling + add mock navigation elements

--- wearables-dashboard/src/components/Topnav/Topnav.tsx
+++ wearables-dashboard/src/components/Topnav/Topnav.tsx
@@ -21,11 +21,14 @@ import SettingsIcon from "@mui/icons-material/Settings";
 
 import "./Topnav.css";
 import { mockUsers, useUser } from "../../services/UserService";
+import { User } from "../../types/types";
+import { useTheme } from "@mui/material/styles";
 
 const TAB_STYLING = {
   "&.Mui-focusVisible": {
     backgroundColor: "rgba(100, 95, 228, 0.32)",
   },
+  minWidth: 60,
 };
 
 export default function Topnav({
@@ -36,11 +39,10 @@ export default function Topnav({
   onTabChange: (tab: string) => void;
 }) {
   const { user, switchUser } = useUser();
-  // const [value, setValue] = useState(selectedTab);
+  const theme = useTheme();
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
   const handleChange = (_: SyntheticEvent, newValue: string) => {
-    // setValue(newValue);
     onTabChange(newValue);
   };
 
@@ -52,15 +54,21 @@ export default function Topnav({
     setAnchorEl(null);
   };
 
+  const handleUserChange = (user: User) => {
+    handleClose();
+    switchUser(user);
+  };
+
   return (
     <Box>
       <AppBar
         color="secondary"
-        elevation={0}
+        elevation={1}
         style={{
           position: "relative",
           borderBottom: "1px solid rgba(0, 0, 0, 0.12)",
         }}
+        className="topnav"
       >
         <Toolbar>
           <Box style={{ display: "flex" }}>
@@ -100,32 +108,63 @@ export default function Topnav({
                 width: "100%",
                 backgroundColor: "#635ee7",
               },
+              ".MuiTabs-flexContainer": {
+                flexWrap: "wrap",
+              },
             }}
             centered={true}
           >
             <Tab
               value="analytics"
-              label="Analytics"
+              // label="Analytics"
+              label={
+                <Typography
+                  sx={{ [theme.breakpoints.down("xs")]: { display: "none" } }}
+                >
+                  Analytics
+                </Typography>
+              }
               sx={TAB_STYLING}
               icon={<AnalyticsIcon />}
             />
             <Tab
               value="connect"
-              label="Connect"
+              // label="Connect"
+              label={
+                <Typography
+                  sx={{ [theme.breakpoints.down("xs")]: { display: "none" } }}
+                >
+                  Connect
+                </Typography>
+              }
               sx={TAB_STYLING}
               icon={<DevicesIcon />}
               disabled
             />
             <Tab
               value="upload"
-              label="Upload"
+              // label="Upload"
+              label={
+                <Typography
+                  sx={{ [theme.breakpoints.down("xs")]: { display: "none" } }}
+                >
+                  Upload
+                </Typography>
+              }
               sx={TAB_STYLING}
               icon={<CloudUploadIcon />}
               disabled
             />
             <Tab
               value="settings"
-              label="Settings"
+              // label="Settings"
+              label={
+                <Typography
+                  sx={{ [theme.breakpoints.down("xs")]: { display: "none" } }}
+                >
+                  Settings
+                </Typography>
+              }
               sx={TAB_STYLING}
               icon={<SettingsIcon />}
             />
@@ -162,7 +201,7 @@ export default function Topnav({
               {mockUsers.map((u) => (
                 <MenuItem
                   key={u.id}
-                  onClick={() => switchUser(u)}
+                  onClick={() => handleUserChange(u)}
                   sx={{
                     backgroundColor: u.id === user?.id ? "#e5e5e5" : "inherit",
                   }}
