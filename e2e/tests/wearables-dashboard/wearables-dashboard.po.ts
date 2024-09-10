c8a9a75cf461b900b254ded206eb028bbc307a33
Vova <volodymyr@luzmo.com>
2024-09-10 09:05:52 +0200

    test(e2e): fixed e2e

--- e2e/tests/wearables-dashboard/wearables-dashboard.po.ts
+++ e2e/tests/wearables-dashboard/wearables-dashboard.po.ts
@@ -4,5 +4,5 @@ export class WearablesDashboardPage extends BasePage {
   public readonly pageAdress = '/wearables-dashboard';
 
   public gotoMainPage = async () => await this.goto(this.pageAdress);
-  public NumberOfStepsChart = () => this.page.getByText('Number of steps by minute', {exact: true});
+  public cell = () => this.page.locator('g > .heattable-rect').first();
 }
